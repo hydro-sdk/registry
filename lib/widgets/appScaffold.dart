fbf8f7d3643f54cb5e4f7b7bfd30160d5c7a45ce
Chris Gibb <chris.gibb@outlook.com>
2021-05-14 22:38:02 -0400

    fix layout

--- lib/widgets/appScaffold.dart
+++ lib/widgets/appScaffold.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class AppScaffold extends StatelessWidget {
+class AppScaffold extends StatefulWidget {
   final Widget child;
   final bool showBackgroundLogoColor;
 
@@ -10,44 +10,68 @@ class AppScaffold extends StatelessWidget {
   });
 
   @override
+  _AppScaffoldState createState() => _AppScaffoldState();
+}
+
+class _AppScaffoldState extends State<AppScaffold> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: false,
         title: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              alignment: Alignment.center,
-              child: Image.asset(
-                "assets/logo.png",
-                fit: BoxFit.contain,
-                width: 45,
-                height: 45,
-              ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    "assets/logo.png",
+                    fit: BoxFit.contain,
+                    width: 45,
+                    height: 45,
+                  ),
+                ),
+                const SizedBox(width: 15),
+                const Text(
+                  "Hydro-SDK Registry",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
             ),
-            const SizedBox(width: 15),
-            const Text(
-              "Hydro-SDK Registry",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            )
+            Row(
+              children: [
+                TextButton(
+                  child: const Text("Login"),
+                  onPressed: () async {},
+                ),
+                const SizedBox(width: 10),
+              ],
+            ),
           ],
         ),
       ),
       body: Stack(
         children: [
-          showBackgroundLogoColor
+          widget.showBackgroundLogoColor
               ? Container(
                   color: const Color(0x75166132),
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height / 2.2,
                 )
               : const SizedBox(),
-          child,
+          widget.child,
         ],
       ),
     );
