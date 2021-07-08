367198b3794e35e6b1c5f84097827177cadcb5cd
Chris Gibb <chris.gibb@outlook.com>
2021-07-08 09:28:39 -0400

    wip prod build and publish

---
+++ lib/main_prod.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hydro_sdk/registry/registryApi.dart';
+import 'package:registry/util/userController.dart';
+import 'package:registry/widgets/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+final registryTestHost = Platform.environment["REGISTRY_TEST_HOST"];
+final registryTestPort =
+    int.tryParse(Platform.environment["REGISTRY_TEST_PORT"] ?? "");
+final registryTestScheme = Platform.environment["REGISTRY_TEST_SCHEME"];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  final registryApi =
+      RegistryApi(
+        scheme: registryTestScheme!,
+        host: registryTestHost!,
+        port: registryTestPort,
+      );
+  final userController = UserController(
+    registryApi: registryApi,
+  );
+  await userController.init();
+
+  await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+  runApp(
+    App(
+      registryApi: registryApi,
+      userController: userController,
+    ),
+  );
+}
