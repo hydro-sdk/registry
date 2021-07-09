aa53303c0bf67a7bcd4c165e575e590baa1179da
Chris Gibb <chris.gibb@outlook.com>
2021-07-09 14:56:45 -0400

    use firebase config in prod entrypoint

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hydro_sdk/registry/registryApi.dart';
+import 'package:js/js_util.dart';
 
+import 'package:registry/firebase.dart';
 import 'package:registry/util/userController.dart';
 import 'package:registry/widgets/app.dart';
 
@@ -14,6 +16,17 @@ final registryTestPort =
 final registryTestScheme = const String.fromEnvironment("REGISTRY_TEST_SCHEME");
 
 Future<void> main() async {
+  initializeApp(jsify({
+    "apiKey": "AIzaSyCpwINU5tsSZ_QRzcHRAuhj-QsiCPTEiSY",
+    "authDomain": "hydro-reservoir-staging.firebaseapp.com",
+    "projectId": "hydro-reservoir-staging",
+    "storageBucket": "hydro-reservoir-staging.appspot.com",
+    "messagingSenderId": "314512543852",
+    "appId": "1:314512543852:web:99fdf42edf468fccb10849",
+    "measurementId": "G-YFTHEJGBPM"
+  }));
+  analytics();
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
