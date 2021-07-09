4ed71ba8e4914ce003a50d5590f0daa0aba5e402
Chris Gibb <chris.gibb@outlook.com>
2021-07-09 13:10:56 -0400

    use dart defines instead of environment variables

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,18 +8,18 @@ import 'package:hydro_sdk/registry/registryApi.dart';
 import 'package:registry/util/userController.dart';
 import 'package:registry/widgets/app.dart';
 
-final registryTestHost = Platform.environment["REGISTRY_TEST_HOST"];
+final registryTestHost = const String.fromEnvironment("REGISTRY_TEST_HOST");
 final registryTestPort =
-    int.tryParse(Platform.environment["REGISTRY_TEST_PORT"] ?? "");
-final registryTestScheme = Platform.environment["REGISTRY_TEST_SCHEME"];
+    int.tryParse(const String.fromEnvironment("REGISTRY_TEST_PORT"));
+final registryTestScheme = const String.fromEnvironment("REGISTRY_TEST_SCHEME");
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   final registryApi = RegistryApi(
-    scheme: registryTestScheme!,
-    host: registryTestHost!,
+    scheme: registryTestScheme,
+    host: registryTestHost,
     port: registryTestPort,
   );
   final userController = UserController(
